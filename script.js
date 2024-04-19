d15a9e7e5a42fac14cbc57605a4527de9c40f90c
I396596 <r.boldewijn@student.fontys.nl>
2024-04-19 12:36:58 +0200

    data set bijgewerkt

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ window.onload = () => {
 
 function filterData() {
   const dropdown1Value = document.getElementById('dropdown1').value;
-  const dropdown2Value = document.getElementById('dropdown2').value;
+  const dropdown2Value = document.getElementById('dropdown2').value;  
+  const dropdown3Value = document.getElementById('dropdown3').value;
 
-  const filteredData = jsonData.filter(item => item.category === dropdown1Value && item.type === dropdown2Value);
+  const filteredData = jsonData.filter(item =>
+    item.category === dropdown1Value && 
+    item.type === dropdown2Value &&
+    item.status === dropdown3Value); 
 
-  // Toon alleen de reviews in plaats van het gehele object
   const reviews = filteredData.map(item => item.review).join("<br>");
   document.getElementById('result').innerHTML = reviews;
 }
